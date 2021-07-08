6eb1bfa1129112178386257f845f1c3505f214d6
luciano riquelme <joker677@hotmail.com.ar>
2021-07-07 21:13:58 -0300

    jaguarete tp

---
+++ js/carrito.js
@@ -0,0 +1,10 @@
+$( "#target" ).click(function(carrito) {
+    //Simple
+    window.location.href = "file:///C:/Users/joker/Desktop/mi%20primer%20sitio%20web/carrito/Carro%20Compras%20JavaScript/index.html";
+    //Pestaña
+    window.open("file:///C:/Users/joker/Desktop/mi%20primer%20sitio%20web/carrito/Carro%20Compras%20JavaScript/index.html");
+    //Ventana
+    window.open
+        ("http://www.google.com", "Tu Titulo", "width=500, height=500")
+
+    });
